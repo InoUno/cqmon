09f7dd4a80de4e9b6d549e69f82924a61a2c57ea
InoUno <inoiown@gmail.com>
2021-05-25 03:49:59 +0200

    UI, kill counter, and better controls

--- cqmon.lua
+++ cqmon.lua
@@ -1,16 +1,62 @@
 chat = require('chat')
 files = require('files')
 packets = require('packets')
+texts = require('texts')
+config = require('config')
 
 _addon = _addon or {}
 _addon.name = 'ConquestMon'
 _addon.author = 'InoUno'
-_addon.version = '1.0.0'
+_addon.version = '2.0.0'
 _addon.command = 'cqmon'
 
 cqmon = cqmon or {}
+cqmon.data = cqmon.data or {}
+cqmon.settings = cqmon.settings or {}
 
-cqmon.mobs = {}
+cqmon.defaults = {}
+cqmon.defaults.infobox = T{
+  pos = T{
+    x = windower.get_windower_settings().x_res - 240,
+    y = windower.get_windower_settings().y_res * 1/4,
+  },
+  padding = 2,
+  text = {
+    size  = 10,
+    font  = 'Consolas',
+    alpha = 255,
+    red   = 255,
+    green = 255,
+    blue  = 255,
+  },
+  bg = {
+    red   = 30,
+    green = 30,
+    blue  = 60,
+    alpha = 230,
+  },
+}
+
+cqmon.onkill = cqmon.onkill or true
+cqmon.auto = cqmon.auto or nil
+
+function cqmon.startAutoQuery(delay)
+  cqmon.print(string.format("Starting auto query with delay of about %d seconds.", delay))
+  cqmon.auto = coroutine.schedule(function ()
+    while true do
+      cqmon.requestConquestMessage()
+      coroutine.sleep(delay + math.random() * 2)
+    end
+  end, delay)
+end
+
+function cqmon.endAutoQuery()
+  if cqmon.auto then
+    cqmon.print("Stopping auto query.")
+    coroutine.close(cqmon.auto)
+    cqmon.auto = nil
+  end
+end
 
 function cqmon.getBits(value, start, length)
   return bit.band(bit.rshift(value, start), bit.lshift(1, length)-1)
@@ -33,8 +79,11 @@ end
 
 -- Handle mob death
 function cqmon.handleDeath(mobId)
-  cqmon.print('Requesting conquest information')
-  cqmon.requestConquestMessage()
+  if cqmon.onkill then
+    cqmon.requestConquestMessage()
+  end
+  cqmon.data.kills = (cqmon.data.kills or 0) + 1
+  cqmon.infobox.kills = cqmon.data.kills
 end
 
 
@@ -51,36 +100,62 @@ function cqmon.handleDefeatMessage(data)
 end
 
 function cqmon.handleConquestMessage(data)
-  -- local packet = packets.parse('incoming', data)
+  local percentages = {
+    sandoria = data:byte(1 + 0x86),
+    bastok = data:byte(1 + 0x87),
+    windurst = data:byte(1 + 0x88),
+    beastmen = data:byte(1 + 0x94),
+  }
 
-  -- for i=1, #data do
-  --   cqmon.print(i .. ': ' .. data:byte(i))
-  -- end
+  local unknown = {
+    sandoria = data:byte(1 + 0x89),
+    bastok = data:byte(1 + 0x8A),
+    windurst = data:byte(1 + 0x8B),
+  }
 
-  cqmon.print('Sandy %: ' .. data:byte(1 + 0x86))
-  cqmon.print('Bastok %: ' .. data:byte(1 + 0x87))
-  cqmon.print('Windy %: ' .. data:byte(1 + 0x88))
-  cqmon.print('Beastmen %: ' .. data:byte(1 + 0x94))
+  cqmon.data.lastQuery = os.time()
+  cqmon.infobox.sandoriaPct = percentages.sandoria
+  cqmon.infobox.bastokPct = percentages.bastok
+  cqmon.infobox.windurstPct = percentages.windurst
+  cqmon.infobox.beastmenPct = percentages.beastmen
 
+  if cqmon.data.lastBeastmenPct ~= percentages.beastmen then
+    cqmon.print(string.format('Beastmen percentage has changed: %d to %d', cqmon.data.lastBeastmenPct, percentages.beastmen))
+    cqmon.data.lastBeastmenPct = percentages.beastmen
+    cqmon.data.lastBeastmenChange = os.time()
+  end
+end
 
-  cqmon.print('Sandy unknown: ' .. data:byte(1 + 0x89))
-  cqmon.print('Bastok unknown: ' .. data:byte(1 + 0x8A))
-  cqmon.print('Windy unknown: ' .. data:byte(1 + 0x8B))
+--------------------
+-- Build UI
+--------------------
 
-  -- for key, value in pairs(fields) do
-  --   cqmon.print(key .. ': ' .. value)
-  -- end
-  -- for key, value in pairs(packet) do
-  --   cqmon.print(key .. ': ' .. value)
-  -- end
+function cqmon.buildInfoBox()
+  if not cqmon.infobox then
+    local lines = {
+      "San d'Oria:     ${sandoriaPct|%d}%",
+      "Bastok:         ${bastokPct|%d}%",
+      "Windurst:       ${windurstPct|%d}%",
+      "Beastmen:       ${beastmenPct|%d}%",
+      "---------------------",
+      "Kills:          ${kills|%d}",
+      "Last query:     ${sinceLastQuery|%d}s",
+    }
+    cqmon.infobox = texts.new(table.concat(lines, '\n'), cqmon.defaults.infobox)
+    cqmon.infobox.kills = 0
+    cqmon.infobox.sandoriaPct = 0
+    cqmon.infobox.bastokPct = 0
+    cqmon.infobox.windurstPct = 0
+    cqmon.infobox.beastmenPct = 0
+    cqmon.infobox.sinceLastQuery = 0
+    cqmon.infobox:show()
+  end
+end
 
-  -- cqmon.print(packet["San d'Oria region bar"])
-  -- cqmon.print(packet["Bastok region bar"])
-  -- cqmon.print(packet["Windurst region bar"])
-  -- cqmon.print(packet["San d'Oria region bar without beastmen"])
-  -- cqmon.print(packet["Bastok region bar without beastmen"])
-  -- cqmon.print(packet["Windurst region bar without beastmen"])
-  -- cqmon.print(packet["Beastmen region bar"])
+function cqmon.prerender()
+  if cqmon.infobox then
+    cqmon.infobox.sinceLastQuery = cqmon.data.lastQuery and (os.time() - cqmon.data.lastQuery) or 0
+  end
 end
 
 --------------------
@@ -156,15 +231,29 @@ end
 -----------------------
 windower.register_event('action', cqmon.actionHandler)
 windower.register_event('incoming chunk', cqmon.chunkHandler)
-windower.register_event('prerender', function()
-  for _, mob in pairs(cqmon.mobs) do
-    cqmon.calculate(mob)
+windower.register_event('prerender', cqmon.prerender)
+
+windower.register_event('addon command', function (command, arg1)
+	command = command and command:lower()
+	if command == 'request' or command == 'r' then
+    cqmon.requestConquestMessage()
+  elseif command == 'auto' or command == 'a' then
+    if cqmon.auto then
+      cqmon.endAutoQuery()
+    else
+      cqmon.startAutoQuery(tonumber(arg1) or 20)
+    end
+  elseif command == 'onkill' or command == 'k' then
+    cqmon.onkill = not cqmon.onkill
+    cqmon.print('Auto query on kill is now: ' .. (cqmon.onkill and 'ON' or 'OFF'))
   end
 end)
 
-windower.register_event('addon command', function (command, ...)
-	command = command and command:lower()
-	if command == 'request' then
-    cqmon.requestConquestMessage()
-	end
+windower.register_event('load', function ()
+  cqmon.settings = config.load(config.defaults)
+  cqmon.buildInfoBox()
+end)
+
+windower.register_event('unload', function ()
+  config.save(cqmon.settings)
 end)
